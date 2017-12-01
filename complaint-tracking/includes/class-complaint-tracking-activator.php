7be3adc4c8bff288e90d1d2527c8e934bf839e7f
DVRtechnohub112 <dvrtechnohub112@gmail.com>
2017-12-01 22:45:40 +0530

    added boiler plat

---
+++ complaint-tracking/includes/class-complaint-tracking-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/DVRtechnohub112
+ * @since      1.0.0
+ *
+ * @package    Complaint_Tracking
+ * @subpackage Complaint_Tracking/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Complaint_Tracking
+ * @subpackage Complaint_Tracking/includes
+ * @author     DVRtechnohub112 <dvrtechnohub112@gmail.com>
+ */
+class Complaint_Tracking_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ complaint-tracking/includes/class-complaint-tracking-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/DVRtechnohub112
+ * @since      1.0.0
+ *
+ * @package    Complaint_Tracking
+ * @subpackage Complaint_Tracking/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Complaint_Tracking
+ * @subpackage Complaint_Tracking/includes
+ * @author     DVRtechnohub112 <dvrtechnohub112@gmail.com>
+ */
+class Complaint_Tracking_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
